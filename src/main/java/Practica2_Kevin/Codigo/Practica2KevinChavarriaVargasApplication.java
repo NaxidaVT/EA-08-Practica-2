ab22744567d3ec1950652621a6a04cb83a62d0f7
NaxidaVT <145503760+NaxidaVT@users.noreply.github.com>
2024-02-14 13:29:36 -0600

    Practica 2 Actualizacion

---
+++ src/main/java/Practica2_Kevin/Codigo/Practica2KevinChavarriaVargasApplication.java
@@ -0,0 +1,13 @@
+package Practica2_Kevin.Codigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2KevinChavarriaVargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2KevinChavarriaVargasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Practica2_Kevin/Codigo/Practica2KevinChavarriaVargasApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica2_Kevin.Codigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2KevinChavarriaVargasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
